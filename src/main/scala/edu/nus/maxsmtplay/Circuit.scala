5c06bb29a41751f3823da0732de937d2e332da60
Sergey Mechtaev <mechtaev@gmail.com>
2013-11-17 08:50:54 +0800

    Circuit is fixed

--- src/main/scala/edu/nus/maxsmtplay/Circuit.scala
+++ src/main/scala/edu/nus/maxsmtplay/Circuit.scala
@@ -9,39 +9,37 @@ import scalaz._
 trait Circuit extends AtMostK {
   this: Z3 =>
 
-  override def atMostK(cs: List[Z3AST], k: Int): Unit = {
-    val n = cs.length
+  override def atMostK(constraints: List[Z3AST], k: Int): Unit = {
+    val n = constraints.length
     if (k >= n || n <= 1)
       return
-    val counterBits = mkCounterCircuit(cs)
+    val counterBits = mkCounterCircuit(constraints)
     assertLEK(counterBits, k)
   }
 
   def assertLEK(value: List[Z3AST], k: Int): Unit = {
-    var not_val = z3.mkNot(value(0))
-    var out = not_val
+    var notVal = z3.mkNot(value(0))
+    var out = notVal
     if (getBit(k, 0))
       out = z3.mkTrue
     var index = 1
-   // value.map({ ast => println("val:" + z3.astToString(ast)) })
     while (index < value.length) {
       var i1 = z3.mkFalse()
       var i2 = z3.mkFalse()
-      // if(value(index).getSort == z3.mkBoolSort)
-      not_val = z3.mkNot(value(index))
+      notVal = z3.mkNot(value(index))
       if (getBit(k, index)) {
-        i1 = not_val
+        i1 = notVal
         i2 = out
       } else {
         i1 = z3.mkFalse()
         i2 = z3.mkFalse()
       }
-      out = mkTernaryOr(i1, i2, z3.mkAnd(not_val, out))
+      out = mkTernaryOr(i1, i2, z3.mkAnd(notVal, out))
       index = index + 1
     }
-    println("at-most-k:")
-    printlnAST(out)
-    solver.context.assertCnstr(out)
+    //println("at-most-k:")
+    //printlnAST(out)
+    solver.assertCnstr(out)
   }
 
   def getBit(value: Int, idx: Int): Boolean = {
@@ -50,112 +48,74 @@ trait Circuit extends AtMostK {
   }
 
   def mkCounterCircuit(cs: List[Z3AST]): List[Z3AST] = {
-    var num_ins = cs.length
+    var numIns = cs.length
     var tempList = cs
-    var aux_2 = List[Z3AST]()
-
-    var num_bits = 1
-    while (num_ins > 1) {
-      val (retList, new_num_ins) = mkAdderPairs(tempList, num_ins, num_bits)
-      aux_2 = retList
-      //tempList = tempList ++ retList
-      //println("tempList size:"+tempList.size)
-      num_ins = new_num_ins
-      num_bits = num_bits + 1
-     // println("num_bits:" + num_bits + " num_ins:" + num_ins)
-      //aux_2.map({ ast => println("bits:" + z3.astToString(ast)) })
-
-      val tmp = tempList
-      tempList = aux_2
-      //tempList.map({ ast => println("after bits:" + z3.astToString(ast)) })
-      aux_2 = tmp
+    var numBits = 1
+    while (numIns > 1) {
+      val (retList, newNumIns) = mkAdderPairs(tempList, numIns, numBits)
+      println("newNumIns: " + newNumIns)
+      numIns = newNumIns
+      numBits = numBits + 1
+      tempList = retList
     }
     tempList
   }
 
-  def mkAdderPairs(in: List[Z3AST], num_ins: Int, num_bits: Int): (List[Z3AST], Int) = {
-    var _out = List[Z3AST]()
+  def mkAdderPairs(in: List[Z3AST], numIns: Int, numBits: Int): (List[Z3AST], Int) = {
+    var output = List[Z3AST]()
     var outCopy = List[Z3AST]()
     var inCopy = List[Z3AST]()
-    var _in = in
+    var input = in
     var index = 0
-    val out_num_bits = num_bits + 1
-    val out_num_ins = half(num_ins)
-    /*
-    def pairList(l: List[Z3AST]):List[Z3AST] = {
-      in match{
-        case a::b::tl => mkAdder(List[Z3AST](a),List[Z3AST](b),num_bits) ++ mkAdderPairs(tl,num_ins,num_bits+1)._1
-        case a::List() => mkAdder(List[Z3AST](a),List[Z3AST](z3.mkFalse()),num_bits)
-        case List() => List()
-      }
-    }
-    pairList(in)
-     */
-
-    while (index < num_ins / 2) {
-      val result = mkAdder(_in, _in.drop(num_bits), num_bits)
-      _out = result
+    val outNumBits = numBits + 1
+    val outNumIns = half(numIns)
+    
+    while (index < numIns / 2) {
+      println("numBits:" + numBits)
+      val result = mkAdder(input, input.drop(numBits), numBits)
+      output = result
       inCopy = inCopy ++ in
       outCopy = outCopy ++ result
-      //result.map({ ast => println("here:"+z3.astToString(ast))})
-      //_out.map({ ast => println("out here:"+z3.astToString(ast))})
-       _in = _in.drop(num_bits)
-       _in = _in.drop(num_bits)
-       _out = _out.drop(out_num_bits)
-       // println("out after size:"+_out.size)
-     // _out.map({ ast => println("out after here:"+z3.astToString(ast))})
+      input = input.drop(numBits * 2)
+      output = output.drop(outNumBits)
       index = index + 1
     }
-    //outCopy = List[Z3AST]()
-    if (num_ins % 2 != 0) {
+    if (numIns % 2 != 0) {
       var i = 0
-      while (i < num_bits) {
-        outCopy = outCopy ++ List(_in(i))
+      while (i < numBits) {
+        outCopy = outCopy ++ List(input(i))
         i = i + 1
       }
       outCopy = outCopy ++ List(z3.mkFalse)
-      //println("outCopy:"+outCopy.size)
-      //_in.map({ ast => println("inCopy here:" + z3.astToString(ast)) })
-      //outCopy.map({ ast => println("out here:" + z3.astToString(ast)) })
-      return (outCopy, out_num_ins)
+      return (outCopy, outNumIns)
     }
-    //println("outsize:"+outCopy.size)
-   // _out.map({ ast => println("here:" + z3.astToString(ast)) })
-    return (outCopy, out_num_ins)
+    return (outCopy, outNumIns)
   }
 
-  def mkAdder(in1: List[Z3AST], in2: List[Z3AST], num_bits: Int): List[Z3AST] = {
-    //in1.map({ ast => println("in1:"+z3.astToString(ast))})
-    //in2.map({ ast => println("in2:"+z3.astToString(ast))})
+  /**
+    * Create an adder of two bit arrays
+    */
+  def mkAdder(in1: List[Z3AST], in2: List[Z3AST], numBits: Int): List[Z3AST] = {
     var cin = z3.mkFalse()
-    var cout = z3.mkFalse()
     var index = 0
     var result = List[Z3AST]()
-    while (index < num_bits) {
-     // println("i: " + index + "num_bits: " + num_bits)
-     // println("in1 " + index + ":" + z3.astToString(in1(index)))
-      //println("in2 " + index + ":" + z3.astToString(in2(index)))
+    while (index < numBits) {
       val (carryout, out) = mkFullAdder(in1(index), in2(index), cin)
-      cout = carryout
       result = result ++ List(out)
-      cin = cout
+      cin = carryout
       index = index + 1
     }
-    result = result ++ List(cout)
-    /*
-    val out = in1.zip(in2).map({case (a,b)=>mkFullAdder(a,b,cin)})
-    val (result, _) = 
-      in1.zip(in2).foldLeft((List[Z3AST](), cin))(
-    		{case ((prevResult, prevAdder), (a, b)) => 
-    		  	  	val (nextResult, nextAdder) = mkFullAdder(a, b, prevAdder)
-    		  	  	(prevResult ++ List(nextResult), nextAdder)
-    		})
-    		*/
+    result = result ++ List(cin)
     result
   }
 
+  /** 
+    * Constructs a full adder for two bits
+    * return a pair of (cout, out)
+    */
   def mkFullAdder(in1: Z3AST, in2: Z3AST, cin: Z3AST): (Z3AST, Z3AST) = {
-    val cout = mkTernaryOr(z3.mkAnd(in1, in2), z3.mkAnd(in1, cin), z3.mkAnd(in2, cin))
+    val cout = 
+      mkTernaryOr(z3.mkAnd(in1, in2), z3.mkAnd(in1, cin), z3.mkAnd(in2, cin))
     val out = z3.mkXor(z3.mkXor(in1, in2), cin)
     (cout, out)
   }
--- src/main/scala/edu/nus/maxsmtplay/Linear.scala
+++ src/main/scala/edu/nus/maxsmtplay/Linear.scala
@@ -3,8 +3,8 @@ package edu.nus.maxsmtplay
 import z3.scala._
 import z3.scala.dsl._
 /**
- * Implementation of linear algorithm
- */
+  * Implementation of linear algorithm
+  */
 trait Linear extends MaxSMT {
   this: Z3 with AtMostK =>
 
@@ -18,37 +18,27 @@ trait Linear extends MaxSMT {
     }
 
     if (soft.size == 0) {
-            println("result: " + 0)
+      println("result: " + 0)
       return List()
 
     }
-    var aux = soft.map(c => (z3.mkFreshConst("k", z3.mkBoolSort)))
-    var softAndAux = soft.zip(aux)
-    //assert soft and aux:
-    var auxPair = softAndAux.map({ case (c, a) => z3.mkOr(c, a) })
-    auxPair.map(solver.assertCnstr)
-    
+    var aux = assertAssumptions(soft).map(_._2)
+
     var k = soft.size - 1
     while (true) {
-     // println("Before....................")
-      //aux.map({ ast => println(z3.astToString(ast))})
-      //println("After.....................")
-      println("checking whether at-most "+k+" soft-constraints can be ignored.")
+      println("checking whether at-most " + k + " soft-constraints can be ignored.")
       atMostK(aux, k)
-      val resultAndModel = z3.checkAndGetModel()
-      val Some(result) = resultAndModel._1
-      val model = resultAndModel._2
+      val Some(result) = solver.check()
       if (!result) {
         println("unsat")
         println("result: " + (soft.size - k - 1))
         return List()
       }
-      //aux.map({ ast => println("aux:"+z3.astToString(ast)) })
+      val model = solver.getModel()
       val numDisabled = getNumDisabledSoftConstraint(model, aux)
       println("number of disabled:" + numDisabled)
       if (numDisabled > k)
         throw new Exception("Number of disabled constraints is more than k")
-     model.delete
       println("sat")
       k = numDisabled
       if (k == 0) {
@@ -57,38 +47,29 @@ trait Linear extends MaxSMT {
       }
       k = k - 1
     }
-    
-    println("result: " + 0)
 
+    println("result: " + 0)
     List()
   }
   /**
-   * Return the number of soft-constraints that were disabled by the given model.
-   * A soft-constraint was disabled if the associated auxiliary variable was assigned to true.
-   */
+    * Return the number of soft-constraints that were disabled by the given model.
+    * A soft-constraint was disabled if the associated auxiliary variable was assigned to true.
+    */
   def getNumDisabledSoftConstraint(model: Z3Model, aux: List[Z3AST]): Int = {
 
     val t = z3.mkTrue
     var disabled = 0
     var i = 0
-    //Some(result)
     while (i < aux.length) {
       val Some(result) = model.eval(aux(i))
       print(aux(i))
       print("->")
       printlnAST(result)
-      //if (result) {
-        if (result.equals(t)) {
-          disabled = disabled + 1
-        }
-      //}
+      if (result.equals(t)) {
+        disabled = disabled + 1
+      }
       i = i + 1
     }
-  /*
-    aux.count((x: Z3AST) => {
-      val Some(result) = model.evalAs[Boolean](x)
-      result && Some(result).eq(t)
-    })*/
     disabled
   }
 
